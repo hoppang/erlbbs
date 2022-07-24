0c5a556e560f8ae88740aac3880aa24f6ab0e89a
Bohun Kim <hoppang@gmail.com>
2022-07-25 01:12:53 +0900

    #8 add logger_colorful module / sys.config / test log code

--- src/erlbbs_app.erl
+++ src/erlbbs_app.erl
@@ -9,7 +9,11 @@
 
 -export([start/2, stop/1]).
 
+-include_lib("kernel/include/logger.hrl").
+
 start(_StartType, _StartArgs) ->
+    ?LOG_NOTICE("Start application"),
+
     %% cowboy router 설정
     Dispatch = cowboy_router:compile([{'_', [{"/", index_controller, []}]}]),
     ListeningPort = 60000,
