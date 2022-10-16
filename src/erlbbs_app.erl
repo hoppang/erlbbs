b1909ec1d486fac5b510772a0b742cb37850437d
Bohun Kim <hoppang@gmail.com>
2022-10-16 22:02:10 +0900

    riak client 사용예

--- src/erlbbs_app.erl
+++ src/erlbbs_app.erl
@@ -18,6 +18,18 @@ start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([{'_', [{"/", index_controller, []}]}]),
     ListeningPort = 60000,
 
+    riak_process:start_link(),
+
+    % 그냥 db 접근 코드 예제..
+    Bucket = <<"Foods">>,
+    Key = <<"Korean">>,
+    gen_server:cast(riak_process, {new, Bucket, Key, <<"Kimchi">>}),
+    Obj = gen_server:call(riak_process, {read, Bucket, Key}),
+    ?LOG_NOTICE("OBJ = ~p~n", [Obj]),
+    gen_server:cast(riak_process, {update, Bucket, Key, <<"Gimbab">>}),
+    Obj2 = gen_server:call(riak_process, {read, Bucket, Key}),
+    ?LOG_NOTICE("OBJ2 = ~p~n", [Obj2]),
+
     %% http server 설정
     {ok, _Pid} =
         cowboy:start_clear(http, [{port, ListeningPort}], #{env => #{dispatch => Dispatch}}),
